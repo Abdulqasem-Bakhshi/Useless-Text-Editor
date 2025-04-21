7a49bc0de5d07ac3ff3b22a5ac11a2b43133a3e7
Abdulqasem-Bakhshi <90260023+abdulqasem-bakhshi@users.noreply.github.com>
2025-04-21 18:53:56 +0200

    basic js functions set

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,112 @@
 window.onload = () => {
-  document.querySelector('.text-editor-input').focus();
+  // Select text editor input element
+  const textEditor = document.querySelector('.text-editor-input');
+
+  // Make text editor input focusable
+  textEditor.focus();
+
+  // Event Listener to ensure onclick maintain focus on text editor
+  document.addEventListener('click', (event) => {
+    if (event.target !== textEditor) {
+      textEditor.focus();
+    }
+  })
+
+  // Select Zoom In and Zoom Out buttons
+  const zoomInTextEditor = document.querySelector('#zoom-in');
+  const zoomOutTextEditor = document.querySelector('#zoom-out');
+
+  // Select current font size of text editor and save
+  let currentFontSize = parseInt(window.getComputedStyle(textEditor).fontSize);
+
+  // Listen to click event of Zoom In + Response
+  document.addEventListener('click', (event) => {
+    if (event.target === zoomInTextEditor) {
+      currentFontSize += 1;
+      textEditor.style.fontSize = `${currentFontSize}px`
+    }
+  });
+  
+  // Listen to click event of Zoom Out + Response
+  document.addEventListener('click', (event) => {
+    if (event.target === zoomOutTextEditor) {
+      currentFontSize -= 1;
+      textEditor.style.fontSize = `${currentFontSize}px`
+    }
+  });
+
+  // Select Undo button
+  const undoTextEditor = document.querySelector('#undo');
+
+  // Listen to click event of Undo + Response
+  document.addEventListener('click', (event) => {
+    if (event.target === undoTextEditor) {
+      const text = textEditor.value.trim().split(' ');
+      const removedText = text.pop();
+      redoStorage.push(removedText);
+      
+      textEditor.value = text.join(' ');
+    }
+  })
+
+  // Select Redo button
+  const redoTextEditor = document.querySelector('#redo');
+  const redoStorage = [];
+
+  // Listen to click event of Redo + Response
+  document.addEventListener('click', (event) => {
+    if (event.target === redoTextEditor) {
+      if (redoStorage.length > 0) {
+        const restoredText = redoStorage.pop();
+        textEditor.value = textEditor.value.trim() + ' ' + restoredText
+      }
+      }
+    }
+  );
+
+  // Select Clear Text button
+  const clearTextButton = document.querySelector('#clear');
+
+  // Listen to click event of Clear Text + Response
+  document.addEventListener('click', (event) => {
+    if (event.target === clearTextButton) {
+      textEditor.value = '';
+    }
+  })
+
+  // Select Open button
+  const openTextButton = document.querySelector('#open');
+  const openFileInput = document.querySelector('#open-file-input');
+
+  // Listen to click event of Open + Response
+  openTextButton.addEventListener('click', () => {
+    openFileInput.click();
+  });
+
+  openFileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        textEditor.value = e.target.result;
+      };
+      reader.readAsText(file);
+    }
+  })
+
+  // Select Save button
+  const saveTextButton = document.querySelector('#save');
+  const saveFileInput = document.querySelector('#save-file-input');
+
+  // Listen to click event of Save + Response
+  saveTextButton.addEventListener('click', () => {
+
+    const text = textEditor.value;
+    const blob = new Blob([text], { type: 'text/plain' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'useless-text-editor.txt';
+    link.click();
+    URL.revokeObjectURL(link.href);
+  });
 }
